c81addd0760ab5d6f692358dc771432ec0cd91be
Martin Nagy <mnagy@redhat.com>
2009-07-31 14:24:12 +0200

    Add CHECK_NEXT() macro similar to CHECK()
    The CHECK_NEXT() macro behaves exactly as the CHECK() macro, with the
    difference that it jumps to the 'next' label. This is useful if we need
    to cleanup after every loop cycle, but don't want to abort the whole
    function if error is found.

--- src/util.h
+++ src/util.h
@@ -21,8 +21,17 @@
 #define _LD_UTIL_H_
 
 #define CHECK(op)						\
-	do { result = (op);					\
-		if (result != ISC_R_SUCCESS) goto cleanup;	\
+	do {							\
+		result = (op);					\
+		if (result != ISC_R_SUCCESS)			\
+			goto cleanup;				\
+	} while (0)
+
+#define CHECK_NEXT(op)						\
+	do {							\
+		result = (op);					\
+		if (result != ISC_R_SUCCESS)			\
+			goto next;				\
 	} while (0)
 
 #define CHECKED_MEM_ALLOCATE(m, target_ptr, s)			\
