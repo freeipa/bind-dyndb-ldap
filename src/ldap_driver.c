freeipa/bind-dyndb-ldap

commit 6b96179255131d693ee372302220a57cb151bbf1

Improve handling of questions which are more than one label beneath zone.
Signed-off-by: Adam Tkac <atkac@redhat.com>

src/ldap_driver.c
@@ -459,6 +459,9 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 	isc_boolean_t is_cname = ISC_FALSE;
 	isc_boolean_t is_delegation = ISC_FALSE;
 	ldapdb_rdatalist_t rdatalist;
+	unsigned int labels, qlabels;
+	dns_fixedname_t fname;
+	dns_name_t *traversename;
 
 	UNUSED(now);
 	UNUSED(options);
@@ -473,15 +476,47 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 	}
 
 	if (!dns_name_issubdomain(name, &db->origin))
-		return DNS_R_NXDOMAIN;	
+		return DNS_R_NXDOMAIN;
 
-	result = ldapdb_rdatalist_get(ldapdb->common.mctx, ldapdb->ldap_inst,
-				      name, &ldapdb->common.origin,
-				      &rdatalist);
+	labels = dns_name_countlabels(&db->origin);
+	qlabels = dns_name_countlabels(name);
+
+	/*
+	 * We need to find the best match in LDAP. We can receive question for
+	 * any name beneath db->origin. We perform "up->down" traverse and
+	 * when we hit authoritative record or delegation point, we stop and
+	 * return answer.
+	 */
+	dns_fixedname_init(&fname);
+	traversename = dns_fixedname_name(&fname);
+
+	if (labels != qlabels) {
+		/*
+		 * Skip the zone label, otherwise we will always find the
+		 * SOA record.
+		 */
+		labels++;
+	}
+
+	while (labels <= qlabels) {
+		dns_name_getlabelsequence(name, qlabels - labels, labels,
+					  traversename);
+
+		result = ldapdb_rdatalist_get(ldapdb->common.mctx,
+					      ldapdb->ldap_inst, traversename,
+					      &ldapdb->common.origin,
+					      &rdatalist);
+		if (result == ISC_R_SUCCESS)
+			goto found;
+
+		labels++;
+	}
+
 
 	if (result != ISC_R_SUCCESS && result != DNS_R_PARTIALMATCH)
 		return (result == ISC_R_NOTFOUND) ? DNS_R_NXDOMAIN : result;
 
+found:
 	/*
 	 * Check if there is at least one NS RR. If yes and this is not NS
 	 * record of this zone (i.e. NS record has higher number of labels),
@@ -492,7 +527,7 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 						&rdlist);
 	if (result == ISC_R_SUCCESS) {
 		if (dns_name_countlabels(&db->origin) <
-		    dns_name_countlabels(name)) {
+		    dns_name_countlabels(traversename)) {
 			/* Delegation point */
 			type = dns_rdatatype_ns;
 			is_delegation = ISC_TRUE;
@@ -531,8 +566,7 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 	}
 
 skipfind:
-	/* XXX currently we implemented only exact authoritative matches */
-	CHECK(dns_name_copy(name, foundname, NULL));
+	CHECK(dns_name_copy(traversename, foundname, NULL));
 
 	if (rdataset != NULL && type != dns_rdatatype_any) {
 		/* dns_rdatalist_tordataset returns success only */
@@ -541,7 +575,7 @@ skipfind:
 	}
 
 	if (nodep != NULL) {
-		CHECK(ldapdbnode_create(ldapdb->common.mctx, name, &node));
+		CHECK(ldapdbnode_create(ldapdb->common.mctx, traversename, &node));
 		memcpy(&node->rdatalist, &rdatalist, sizeof(rdatalist));
 		*nodep = node;
 	} else {

commit 18e43ef8cacb8d0dbda578f08ec3187b1bb4b6a8

When traversing space beneath the zone, return only delegation RRs.
This fixes regression introduced by

6b96179255131d693ee372302220a57cb151bbf1
Improve handling of questions which are more than one label beneath zone.

The plugin incorrectly returned also non-delegation RRs on the traverse path.

Signed-off-by: Adam Tkac <atkac@redhat.com>

src/ldap_driver.c
@@ -484,19 +484,12 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 	/*
 	 * We need to find the best match in LDAP. We can receive question for
 	 * any name beneath db->origin. We perform "up->down" traverse and
-	 * when we hit authoritative record or delegation point, we stop and
-	 * return answer.
+	 * when we hit delegation point, we stop and return an answer.
+	 * If we don't find any delegation, during the path, we work with the
+	 * full match and return answer or NXDOMAIN.
 	 */
 	dns_fixedname_init(&fname);
 	traversename = dns_fixedname_name(&fname);
-
-	if (labels != qlabels) {
-		/*
-		 * Skip the zone label, otherwise we will always find the
-		 * SOA record.
-		 */
-		labels++;
-	}
 
 	while (labels <= qlabels) {
 		dns_name_getlabelsequence(name, qlabels - labels, labels,
@@ -506,38 +499,45 @@ find(dns_db_t *db, dns_name_t *name, dns_dbversion_t *version,
 					      ldapdb->ldap_inst, traversename,
 					      &ldapdb->common.origin,
 					      &rdatalist);
-		if (result == ISC_R_SUCCESS)
-			goto found;
+		if (result == ISC_R_SUCCESS) {
+			/*
+			 * Check if there is at least one NS RR. If yes and this is not NS
+			 * record of this zone (i.e. NS record has higher number of labels),
+			 * we hit delegation point. Delegation point has the highest priority
+			 * and we supress all other RR types than NS.
+			 */
+			result = ldapdb_rdatalist_findrdatatype(&rdatalist,
+								dns_rdatatype_ns,
+								&rdlist);
+			if (result == ISC_R_SUCCESS) {
+				if (dns_name_countlabels(&db->origin) <
+				    dns_name_countlabels(traversename)) {
+					/* Delegation point */
+					type = dns_rdatatype_ns;
+					is_delegation = ISC_TRUE;
+					goto skipfind;
+				} else {
+					/* Our NS RRset, continue as usual */
+					rdlist = NULL;
+				}
+			}
+
+			if (labels == qlabels) {
+				/* We've found an answer */
+				goto found;
+			} else {
+				ldapdb_rdatalist_destroy(ldapdb->common.mctx,
+							 &rdatalist);
+			}
+		}
 
 		labels++;
 	}
 
-
-	if (result != ISC_R_SUCCESS && result != DNS_R_PARTIALMATCH)
+	if (result != ISC_R_SUCCESS)
 		return (result == ISC_R_NOTFOUND) ? DNS_R_NXDOMAIN : result;
 
 found:
-	/*
-	 * Check if there is at least one NS RR. If yes and this is not NS
-	 * record of this zone (i.e. NS record has higher number of labels),
-	 * we hit delegation point. Delegation point has the highest priority
-	 * and we supress all other RR types than NS.
-	 */
-	result = ldapdb_rdatalist_findrdatatype(&rdatalist, dns_rdatatype_ns,
-						&rdlist);
-	if (result == ISC_R_SUCCESS) {
-		if (dns_name_countlabels(&db->origin) <
-		    dns_name_countlabels(traversename)) {
-			/* Delegation point */
-			type = dns_rdatatype_ns;
-			is_delegation = ISC_TRUE;
-			goto skipfind;
-		} else {
-			/* Our NS RRset, continue as usual */
-			rdlist = NULL;
-		}
-	}
-
 	/*
 	 * ANY pseudotype indicates the whole node, skip routines
 	 * which attempts to find the exact RR type.