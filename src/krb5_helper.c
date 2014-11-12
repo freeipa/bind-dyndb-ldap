8ad9965136ab15f14cdb356a81a141575b2a84aa
Petr Spacek <pspacek@redhat.com>
2014-11-12 16:30:56 +0100

    Replace deprecated macro #define _BSD_SOURCE with _POSIX_C_SOURCE.
    See
    https://sourceware.org/glibc/wiki/Release/2.20#Packaging_Changes

--- src/krb5_helper.c
+++ src/krb5_helper.c
@@ -15,7 +15,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#define _BSD_SOURCE
+#define _POSIX_C_SOURCE 200112L /* setenv */
 
 #include <isc/util.h>
 #include <string.h>
