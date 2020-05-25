962179e606c04745080e6da17036dd5793dfb474
Thomas Woerner <twoerner@redhat.com>
2020-05-25 06:41:39 +0000

    bind-9.16: findzonecut and dns_db_findzonecut has additional parameter
    bind9 commit:
    QNAME miminimization should create a separate fetch context for each fetch -
    this makes the cache more efficient and eliminates duplicates queries.

--- src/ldap_driver.c
+++ src/ldap_driver.c
@@ -352,14 +352,15 @@ find(dns_db_t *db, const dns_name_t *name, dns_dbversion_t *version,
 static isc_result_t
 findzonecut(dns_db_t *db, const dns_name_t *name, unsigned int options,
 	    isc_stdtime_t now, dns_dbnode_t **nodep, dns_name_t *foundname,
-	    dns_rdataset_t *rdataset, dns_rdataset_t *sigrdataset)
+	    dns_name_t *dcname, dns_rdataset_t *rdataset,
+	    dns_rdataset_t *sigrdataset)
 {
 	ldapdb_t *ldapdb = (ldapdb_t *) db;
 
 	REQUIRE(VALID_LDAPDB(ldapdb));
 
 	return dns_db_findzonecut(ldapdb->rbtdb, name, options, now, nodep,
-				  foundname, rdataset, sigrdataset);
+				  foundname, dcname, rdataset, sigrdataset);
 }
 
 static void
